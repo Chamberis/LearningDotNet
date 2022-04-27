2b87b6bc3c22fd551af8a87ee4d5d5d779ae710b
chamberis <karolisjakstas@gmail.com>
2022-04-27 16:07:02 +0300

    addig code

---
+++ LearningArrays/LearningArrays/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace LearningArrays
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] newArray = { "Jimmy", "Tom", "Charles", "Eve", "Tim" };
+
+
+            foreach (string name in newArray)
+            {                                          
+                Console.WriteLine(name);
+            }
+                Console.ReadLine();
+
+
+            int[] otherArray = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20};
+
+            for (var i = otherArray.Length - 1 ; i > -1; i--)
+            {
+                Console.WriteLine(i);
+            }
+                Console.ReadLine();
+            }
+            
+    }
+}
